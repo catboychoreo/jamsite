edf35ab6f60da45eebd77940ed1730d198c0b073
catboychoreo <135279465+catboychoreo@users.noreply.github.com>
2024-05-14 20:29:22 -0500

    index.js
    Probably a lot changed i dont know i got rid of evil mode and went to summer idk idk  idk

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // countdown
-const countdownDate = new Date("Apr 13, 2024 16:13:00").getTime()
+const countdownDate = new Date("May 23, 2024 15:20:00").getTime()
 const x = setInterval(function () {
   const now = new Date().getTime();
   const distance = countdownDate - now;
@@ -11,7 +11,7 @@ const x = setInterval(function () {
     + minutes + "m " + seconds + "s ";
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("countdownclock").innerHTML = "A young man stands in his bedroom."
+    document.getElementById("countdownclock").innerHTML = "School's out!"
   }
 }, 1000);
 
@@ -32,9 +32,6 @@ likeButton.addEventListener('click', () => {
 var center = document.getElementById('center')
 var center2 = document.getElementById('center2')
 var center3 = document.getElementById('center3')
-var centerEvil = document.getElementById('center-evil')
-var center2Evil = document.getElementById('center2-evil')
-var center3Evil = document.getElementById('center3-evil')
 
 
 //color changers 
@@ -44,9 +41,6 @@ colors.oninput = function () {
   center.style.backgroundColor = colors.value
   center2.style.backgroundColor = colors.value
   center3.style.backgroundColor = colors.value
-  centerEvil.style.backgroundColor = colors.value
-  center2Evil.style.backgroundColor = colors.value
-  center3Evil.style.backgroundColor = colors.value
 };
 
 var reset = document.getElementById('reset-background')
@@ -55,9 +49,6 @@ reset.onclick = function () {
   center.style.backgroundColor = 'white'
   center2.style.backgroundColor = 'white'
   center3.style.backgroundColor = 'white'
-  centerEvil.style.backgroundColor = 'red'
-  center2Evil.style.backgroundColor = 'red'
-  center3Evil.style.backgroundColor = 'red'
 };
 
 var colorstwo = document.getElementById('colorpick2')
@@ -66,9 +57,6 @@ colorstwo.oninput = function () {
   center.style.border = 'solid' + colorstwo.value
   center2.style.border = 'solid' + colorstwo.value
   center3.style.border = 'solid' + colorstwo.value
-  centerEvil.style.border = 'solid' + colorstwo.value
-  center2Evil.style.border = 'solid' + colorstwo.value
-  center3Evil.style.border = 'solid' + colorstwo.value
 };
 
 var resettwo = document.getElementById('reset-border')
@@ -77,9 +65,6 @@ resettwo.onclick = function () {
   center.style.border = 'solid black'
   center2.style.border = 'solid black'
   center3.style.border = 'solid black'
-  centerEvil.style.border = 'solid black'
-  center2Evil.style.border = 'solid black'
-  center3Evil.style.border = 'solid black'
 };
 
 // fish code
@@ -116,156 +101,3 @@ fishyFishy.onclick = function () {
   clearInterval(moveInterval)
   fishyFishy.style.left = '-600px'
 };
-
-//variables 4 site changers
-var evilIncarnate = document.getElementById('evil-button')
-var niceIncarnate = document.getElementById('nice-button')
-var evil = document.getElementById('evil')
-var nice = document.getElementById('nice')
-
-nice.style.display = 'block';
-evil.style.display = 'none';
-
-evilIncarnate.onclick = superCool;
-niceIncarnate.onclick = superCool;
-
-function scrollToTop () {
-  window.scrollTo(0, 0);
-}
-
-function superCool () {
-  if (nice.style.display === 'none'){
-    // normal 
-    evil.style.display = 'none'
-    nice.style.display = 'block'
-
-    document.body.style.backgroundColor = 'white'
-    document.body.style.border = 'dotted black'
-    document.body.style.webkitTextStroke = '.05px black'
-  
-    center.style.backgroundColor = 'white'
-    center.style.border = 'black solid'
-    center2.style.backgroundColor = 'white'
-    center2.style.border = 'black solid'
-    center3.style.backgroundColor = 'white'
-    center3.style.border = 'black solid'
-
-  } else {
-    // evil
-    evil.style.display = 'block'
-    nice.style.display = 'none'
-
-    document.body.style.backgroundColor = '#FF0000'
-    document.body.style.border = 'dotted black'
-    document.body.style.webkitTextStroke = '.05px black'
-  
-    center.style.backgroundColor = 'red'
-    center.style.border = 'black solid'
-    center2.style.backgroundColor = 'red'
-    center2.style.border = 'black solid'
-    center3.style.backgroundColor = 'red'
-    center3.style.border = 'black solid'
-  }
-}
-
-//board
-var blockSize = 25;
-var rows = 20;
-var cols = 20;
-var board;
-var context;
-
-//snake
-var snakeX = blockSize * 5;
-var snakeY = blockSize * 5;
-
-var velocityX = 0;
-var velocityY = 0;
-
-var snakeBody = [];
-
-//food
-var foodX;
-var foodY;
-
-var gameOver = false;
-
-window.onload = function () {
-    board = document.getElementById("board");
-    board.height = rows * blockSize;
-    board.width = cols * blockSize;
-    context = board.getContext("2d"); //draws on the board yasss artist
-
-    placeFood();
-    document.addEventListener("keyup", changeDirection);
-    //update();
-    setInterval(update, 800/10);
-}
-
-function update() {
-    if (gameOver){
-        return;
-    }
-    context.fillStyle = "black";
-    context.fillRect(0, 0, board.width, board.height);
-
-    context.fillStyle="red";
-    context.fillRect(foodX,foodY, blockSize, blockSize);
-
-    if (snakeX == foodX && snakeY == foodY) {
-        snakeBody.push([foodX, foodY])
-        placeFood();
-    }
-
-    for (let i = snakeBody.length-1; i > 0; i--){
-        snakeBody[i] = snakeBody[i-1];
-    }
-    if (snakeBody.length) {
-        snakeBody[0] = [snakeX, snakeY];
-    }
-
-    context.fillStyle="lime";
-    snakeX += velocityX * blockSize;
-    snakeY += velocityY * blockSize;
-    context.fillRect(snakeX, snakeY, blockSize, blockSize);
-    for (let i = 0; i < snakeBody.length; i++) {
-        context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
-    }
-
-    if (snakeX < 0 || snakeX > cols*blockSize || snakeY < 0 || snakeY > rows*blockSize) {
-        gameOver = true;
-        alert("you are a little tiny baby who crashed into the wall. GAME OVER");
-    }
-
-    for (let i = 0; i < snakeBody.length; i++){
-        if (snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]){
-            gameOver = true;
-            alert("you are a little tiny baby who ate itself. GAME OVER");
-        }
-    }
-}
-
-function changeDirection(e) {
-    if (e.code == "KeyW" && velocityY != 1) {
-        velocityX = 0;
-        velocityY = -1;
-    }
-    else if (e.code == "KeyS" && velocityY != -1) {
-        velocityX = 0;
-        velocityY = 1;
-    }
-    else if (e.code == "KeyA" && velocityX != 1) {
-        velocityX = -1;
-        velocityY = 0;
-    }
-    else if (e.code == "KeyD" && velocityX != -1) {
-        velocityX = 1;
-        velocityY = 0;
-    }
-}
-
-function placeFood() {
-    foodX = Math.floor(Math.random() * cols) * blockSize;
-    foodY = Math.floor(Math.random() * cols) * blockSize;
-}
-
